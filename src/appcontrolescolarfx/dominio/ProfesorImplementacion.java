31cb9832f5428726c37418545c400f736f4317f2
BloodyRegin4 <marianasv628@gmail.com>
2025-11-01 21:38:01 -0600

    Implementation of editing teacher information

--- src/appcontrolescolarfx/dominio/ProfesorImplementacion.java
+++ src/appcontrolescolarfx/dominio/ProfesorImplementacion.java
@@ -11,35 +11,38 @@ import java.util.LinkedHashMap;
 
 public class ProfesorImplementacion {
     public static HashMap<String, Object> obtenerProfesores(){
-    HashMap<String, Object> respuesta = new LinkedHashMap<>();
-    try{
-        ResultSet resultado = ProfesorDAO.obtenerProfesores(ConexionBD.abrirConexionBD());
-        ArrayList<Profesor> profesores = new ArrayList<>();
-        while (resultado.next()){
-            Profesor profesor = new Profesor();
-            profesor.setIdProfesor(resultado.getInt("idProfesor"));
-            profesor.setIdRol(resultado.getInt("idRol"));
-            profesor.setRol(resultado.getString("rol"));
-            profesor.setNombre(resultado.getString("nombre"));
-            profesor.setApellidoPaterno(resultado.getString("apellidoPaterno"));
-            profesor.setApellidoMaterno(resultado.getString("apellidoMaterno"));
-            profesor.setNoPersonal(resultado.getString("noPersonal"));
-            profesor.setFechaNacimiento(resultado.getString("fechaNacimiento"));
-            profesor.setFechaContratacion(resultado.getString("fechaContratacion"));
-            
-            profesores.add(profesor);
-        }
-        respuesta.put("error", false);
-        respuesta.put("profesores", profesores);
+        HashMap<String, Object> respuesta = new LinkedHashMap<>();
         
-        ConexionBD.cerrarConexionBD();
-    } catch (SQLException e){
-        respuesta.put("error", true);
-        respuesta.put("mensaje", e.getMessage());
+        try{
+            ResultSet resultado = ProfesorDAO.obtenerProfesores(ConexionBD.abrirConexionBD());
+            ArrayList<Profesor> profesores = new ArrayList<>();
+            while (resultado.next()){
+                Profesor profesor = new Profesor();
+                profesor.setIdProfesor(resultado.getInt("idProfesor"));
+                profesor.setIdRol(resultado.getInt("idRol"));
+                profesor.setRol(resultado.getString("rol"));
+                profesor.setNombre(resultado.getString("nombre"));
+                profesor.setApellidoPaterno(resultado.getString("apellidoPaterno"));
+                profesor.setApellidoMaterno(resultado.getString("apellidoMaterno"));
+                profesor.setNoPersonal(resultado.getString("noPersonal"));
+                profesor.setPassword(resultado.getString("password"));
+                profesor.setFechaNacimiento(resultado.getString("fechaNacimiento"));
+                profesor.setFechaContratacion(resultado.getString("fechaContratacion"));
+                
+                profesores.add(profesor);
+            }
+            
+            respuesta.put("error", false);
+            respuesta.put("profesores", profesores);
+            
+            ConexionBD.cerrarConexionBD();
+        } catch (SQLException e){
+            respuesta.put("error", true);
+            respuesta.put("mensaje", e.getMessage());
+        }
+        
+        return respuesta;
     }
-    
-    return respuesta;
-   }
     
     public static HashMap<String, Object> registrarProfesor(Profesor profesor) {
         HashMap<String,Object> respuesta = new LinkedHashMap<>();
@@ -62,4 +65,26 @@ public class ProfesorImplementacion {
         
         return respuesta;
     }
+
+    public static HashMap<String,Object> editarProfesor(Profesor profesor) {
+        HashMap<String,Object> respuesta = new LinkedHashMap<>();
+
+        try {
+            int filasAfectadas = ProfesorDAO.editarProfesor(profesor, ConexionBD.abrirConexionBD();
+
+            if(filasAfectadas > 0) {
+                respuesta.put("error", false);
+                respuesta.put("mensaje", "El registro del profesor(a) " + profesor.getNombre() + " fue editada correctamente.");
+            } else {
+                respuesta.put("error", true);
+                respuesta.put("mensaje", "Lo sentimos :( no se pudo editar la información del profesfor, por favor intente más tarde.")ñ
+            }
+
+            ConexionBD.cerrarConexionBD();
+        } catch (SQLException ex) {
+            respuesta.put("error", true);
+            respuesta.put("mensaje", e.getMessage());
+        }
+
+        return respuesta;
 }

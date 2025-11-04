cf70582ed3af1e9b4c11a88e931e1264226520ba
Tresdese <super_diegobros@hotmail.com>
2025-11-04 14:08:05 -0600

    Se modificaron dependencias

---
+++ src/appcontrolescolarfx/controlador/FXMLAdminAlumnoController.java
@@ -0,0 +1,14 @@
+package appcontrolescolarfx.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLAdminAlumnoController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
